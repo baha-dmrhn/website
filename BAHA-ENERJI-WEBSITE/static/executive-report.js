e0d99ec19740bf7c02a2d1c32c3bb09f50c5fc08
baha-dmrhn <demirhanbaha4@gmail.com>
2026-07-27 16:53:03 +0300

    Delete BAHA-ENERJI-WEBSITE directory

--- BAHA-ENERJI-WEBSITE/static/executive-report.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  "use strict";
-  const printButton = document.querySelector("#reportPrint");
-  printButton?.addEventListener("click", () => window.print());
-  if (document.body.dataset.autoPrint === "true") {
-    window.addEventListener("load", () => window.setTimeout(() => window.print(), 350));
-  }
-})();
